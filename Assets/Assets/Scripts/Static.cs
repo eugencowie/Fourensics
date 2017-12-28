125e92e77a56122c9082888062810b5d638bc411
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-12-28 15:13:00 +0000

    Fix issue in static script

--- Assets/Assets/Scripts/Static.cs
+++ Assets/Assets/Scripts/Static.cs
@@ -5,14 +5,14 @@ using Firebase.Unity.Editor;
 static class Static
 {
     public static FirebaseApp FirebaseApp => FirebaseApp.DefaultInstance;
-    public static FirebaseDatabase FirebaseDatabase => Setup(FirebaseDatabase.DefaultInstance);
+    public static FirebaseDatabase FirebaseDatabase => GetFirebaseDatabase();
 
-    static FirebaseDatabase Setup(FirebaseDatabase database)
+    static FirebaseDatabase GetFirebaseDatabase()
     {
         FirebaseApp.SetEditorDatabaseUrl("https://fourensics-app.firebaseio.com/");
         FirebaseApp.SetEditorP12FileName("db-access-token.p12");
         FirebaseApp.SetEditorServiceAccountEmail("fourensics-app@appspot.gserviceaccount.com");
         FirebaseApp.SetEditorP12Password("notasecret");
-        return database;
+        return FirebaseDatabase.DefaultInstance;
     }
 }

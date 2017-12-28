2431db87fd7daf40766541a693fced1b061e91d5
Eugén Cowie <eugencowie@users.noreply.github.com>
2017-12-28 06:45:37 +0000

    Add static class

---
+++ Assets/Assets/Scripts/Static.cs
@@ -0,0 +1,18 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+static class Static
+{
+    public static FirebaseApp FirebaseApp => FirebaseApp.DefaultInstance;
+    public static FirebaseDatabase FirebaseDatabase => Setup(FirebaseDatabase.DefaultInstance);
+
+    static FirebaseDatabase Setup(FirebaseDatabase database)
+    {
+        FirebaseApp.SetEditorDatabaseUrl("https://fourensics-app.firebaseio.com/");
+        FirebaseApp.SetEditorP12FileName("db-access-token.p12");
+        FirebaseApp.SetEditorServiceAccountEmail("fourensics-app@appspot.gserviceaccount.com");
+        FirebaseApp.SetEditorP12Password("notasecret");
+        return database;
+    }
+}

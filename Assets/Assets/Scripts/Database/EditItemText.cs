aa79651a8c7a76a34459c34f9ebba3f4abd78cdc
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-02-01 15:35:17 +0000

    Add edit item text screen

---
+++ Assets/Assets/Scripts/Database/EditItemText.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+class EditItemText : MonoBehaviour
+{
+    [SerializeField] InputField m_inputField = null;
+    [SerializeField] Button m_submitButton = null;
+    [SerializeField] Button m_cancelButton = null;
+}

46a1c8b48228ea91c7ac09cae50f779a01cd16e3
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-03-15 09:10:14 +0000

    Add M714 utility batch scripts

---
+++ Utilities/M714-Install.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set JAVA_HOME=C:\SDKs\Java\jdk1.8.0_151
+
+C:\SDKs\Android\sdk\tools\bin\sdkmanager.bat platforms;android-26
---
+++ Utilities/M714-Run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set JAVA_HOME=C:\SDKs\Java\jdk1.8.0_151
+
+"C:\Program Files\Unity\Editor\Unity.exe"

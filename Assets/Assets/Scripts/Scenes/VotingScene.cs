e70bd70707b14ad46c9669fc3df13202df894764
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-03-29 11:38:00 +0100

    Display different suspects for each case

--- Assets/Assets/Scripts/Scenes/VotingScene.cs
+++ Assets/Assets/Scripts/Scenes/VotingScene.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -16,69 +18,83 @@ public static class StaticSuspects
 
 public class VotingScene : MonoBehaviour
 {
+    [Serializable]
+    struct Case
+    {
+        public GameObject Container;
+        public VotingSuspect[] Suspects;
+    }
+
     [SerializeField] private GameObject ResetButton = null;
     [SerializeField] private GameObject ReturnButton = null;
     //[SerializeField] private GameObject VoteButton = null;
     [SerializeField] private GameObject[] Backgrounds = new GameObject[4];
-    [SerializeField] private VotingSuspect[] Suspects = new VotingSuspect[8];
+    [SerializeField] private Case[] m_cases = new Case[2];
 
     private string m_lobbyCode;
     private int m_scene;
 
     async void Start()
     {
-        User m_user; try { m_user = await User.Get(); } catch { SceneManager.LoadScene("SignIn"); return; }
-        Lobby m_lobby = await Lobby.Get(m_user);
+        // Get database objects
+        User user; try { user = await User.Get(); } catch { SceneManager.LoadScene("SignIn"); return; }
+        Lobby lobby = await Lobby.Get(user);
 
-        if (m_lobby == null)
+        int scene = (int)(CloudManager.OnlyUser(lobby, user).Scene.Value ?? 0);
+        int caseNb = (int)(lobby.Case.Value ?? 0);
+
+        if (lobby == null || string.IsNullOrEmpty(lobby.Id) || scene <= 0 || caseNb < 1 || caseNb > 2 || (caseNb - 1) >= m_cases.Length)
         {
             SceneManager.LoadScene("Lobby");
             return;
         }
 
+        foreach (var x in m_cases) x.Container.SetActive(false);
+        m_cases[caseNb - 1].Container.SetActive(true);
+
         ResetButton.SetActive(false);
         ReturnButton.SetActive(false);
         //VoteButton.SetActive(false);
 
-        int scene = (int)(CloudManager.OnlyUser(m_lobby, m_user).Scene.Value ?? 0);
-        if (scene > 0)
-        {
-            m_scene = scene;
-            SetBackground();
-            string lobby = m_lobby.Id;
-            if (!string.IsNullOrEmpty(lobby))
-            {
-                m_lobbyCode = lobby;
-                ResetButton.SetActive(true);
-                ReturnButton.SetActive(true);
-                //VoteButton.SetActive(true);
-                DiscardSuspects();
-            }
-            else SceneManager.LoadScene("Lobby");
-        }
-        else SceneManager.LoadScene("Lobby");
+        m_scene = scene;
+        SetBackground();
+        m_lobbyCode = lobby.Id;
+        ResetButton.SetActive(true);
+        ReturnButton.SetActive(true);
+        //VoteButton.SetActive(true);
+        await DiscardSuspects();
     }
 
-    private void DiscardSuspects()
+    async Task DiscardSuspects()
     {
         foreach (var discarded in StaticSuspects.DiscardedSuspects)
         {
-            foreach (var suspect in Suspects)
+            // Get database objects
+            User user; try { user = await User.Get(); } catch { SceneManager.LoadScene("SignIn"); return; }
+            Lobby lobby = await Lobby.Get(user);
+
+            // Get lobby case number
+            int caseNb = (int)(lobby.Case.Value ?? 0);
+
+            if (caseNb >= 1 && caseNb <= 2 && (caseNb - 1) < m_cases.Length)
             {
-                if (suspect.Name.text == discarded.Name)
+                foreach (var suspect in m_cases[caseNb - 1].Suspects)
                 {
-                    var page = suspect.GetComponent<VotingPageController>();
+                    if (suspect.Name.text == discarded.Name)
+                    {
+                        var page = suspect.GetComponent<VotingPageController>();
 
-                    var prevPage = page.PanelLeft;
-                    var nextPage = page.PanelRight;
+                        var prevPage = page.PanelLeft;
+                        var nextPage = page.PanelRight;
 
-                    prevPage.GetComponent<VotingPageController>().PanelRight = nextPage;
-                    nextPage.GetComponent<VotingPageController>().PanelLeft = prevPage;
+                        prevPage.GetComponent<VotingPageController>().PanelRight = nextPage;
+                        nextPage.GetComponent<VotingPageController>().PanelLeft = prevPage;
 
-                    // TODO: don't remove last item
+                        // TODO: don't remove last item
 
-                    page.Right();
-                    suspect.gameObject.SetActive(false);
+                        page.Right();
+                        suspect.gameObject.SetActive(false);
+                    }
                 }
             }
         }
@@ -119,39 +135,49 @@ public class VotingScene : MonoBehaviour
         }
     }
 
-    public void ResetButtonPressed()
+    public async void ResetButtonPressed()
     {
         if (ResetButton.activeSelf)
         {
             ResetButton.SetActive(false);
 
-            for (int i = 0; i < Suspects.Length; i++)
+            // Get database objects
+            User user; try { user = await User.Get(); } catch { SceneManager.LoadScene("SignIn"); return; }
+            Lobby lobby = await Lobby.Get(user);
+
+            // Get lobby case number
+            int caseNb = (int)(lobby.Case.Value ?? 0);
+
+            if (caseNb >= 1 && caseNb <= 2 && (caseNb - 1) < m_cases.Length)
             {
-                int prevIdx = i - 1;
-                int nextIdx = i + 1;
-
-                if (prevIdx < 0)
-                    prevIdx = Suspects.Length - 1;
-
-                if (nextIdx >= Suspects.Length)
-                    nextIdx = 0;
-
-                var current = Suspects[i];
-
-                var prev = Suspects[prevIdx];
-                var next = Suspects[nextIdx];
-
-                var page = current.gameObject.GetComponent<VotingPageController>();
-
-                page.PanelLeft = prev.gameObject;
-                page.PanelRight = next.gameObject;
-
-                // reset opacity
-                if (current.Slot != null)
+                for (int i = 0; i < m_cases[caseNb - 1].Suspects.Length; i++)
                 {
-                    var color = current.Slot.GetComponent<Image>().color;
-                    color.a = 1.0f;
-                    current.Slot.GetComponent<Image>().color = color;
+                    int prevIdx = i - 1;
+                    int nextIdx = i + 1;
+
+                    if (prevIdx < 0)
+                        prevIdx = m_cases[caseNb - 1].Suspects.Length - 1;
+
+                    if (nextIdx >= m_cases[caseNb - 1].Suspects.Length)
+                        nextIdx = 0;
+
+                    var current = m_cases[caseNb - 1].Suspects[i];
+
+                    var prev = m_cases[caseNb - 1].Suspects[prevIdx];
+                    var next = m_cases[caseNb - 1].Suspects[nextIdx];
+
+                    var page = current.gameObject.GetComponent<VotingPageController>();
+
+                    page.PanelLeft = prev.gameObject;
+                    page.PanelRight = next.gameObject;
+
+                    // reset opacity
+                    if (current.Slot != null)
+                    {
+                        var color = current.Slot.GetComponent<Image>().color;
+                        color.a = 1.0f;
+                        current.Slot.GetComponent<Image>().color = color;
+                    }
                 }
             }
 
@@ -164,11 +190,21 @@ public class VotingScene : MonoBehaviour
         User m_user; try { m_user = await User.Get(); } catch { SceneManager.LoadScene("SignIn"); return; }
         Lobby m_lobby = await Lobby.Get(m_user);
 
-        var current = Suspects.First(s => s.gameObject.activeSelf);
-        if (current != null)
+        // Get database objects
+        User user; try { user = await User.Get(); } catch { SceneManager.LoadScene("SignIn"); return; }
+        Lobby lobby = await Lobby.Get(user);
+
+        // Get lobby case number
+        int caseNb = (int)(lobby.Case.Value ?? 0);
+
+        if (caseNb >= 1 && caseNb <= 2 && (caseNb - 1) < m_cases.Length)
         {
-            CloudManager.OnlyUser(m_lobby, m_user).Vote.Value = current.Name.text;
-            SceneManager.LoadScene("VotingWait");
+            var current = m_cases[caseNb - 1].Suspects.First(s => s.gameObject.activeSelf);
+            if (current != null)
+            {
+                CloudManager.OnlyUser(m_lobby, m_user).Vote.Value = current.Name.text;
+                SceneManager.LoadScene("VotingWait");
+            }
         }
     }
 }

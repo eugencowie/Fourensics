2ce9326eceb64821907bced72d0d07ff4cbfd7c6
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-04-05 17:21:40 +0100

    Notify when lobby started

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -66,3 +66,34 @@ exports.playerLeftLobby = functions.database.ref('/lobbies/{id}/users/{uid}/user
     });
 
 });
+
+exports.lobbyStarted = functions.database.ref('/lobbies/{id}/state').onUpdate((snapshot, context) => {
+
+    const getLobbyUserIds = "0123".split('')
+        .map(x => admin.database().ref(`/lobbies/${context.params.id}/users/${x}/user-id`).once('value'));
+
+    return Promise.all(getLobbyUserIds).then(results => {
+
+        const getUserNotificationTokens = results
+            .map(x => admin.database().ref(`/users/${x.val()}/notification-token`).once('value'));
+
+        return Promise.all(getUserNotificationTokens);
+
+    }).then(results => {
+
+        const notificationTokens = results
+            .map(x => x.val())
+            .filter(x => !!x);
+
+        const payload = {
+            notification: {
+                title: 'The game has started!',
+                body: 'The lobby has been started!'
+            }
+        };
+
+        return admin.messaging().sendToDevice(notificationTokens, payload);
+
+    });
+
+});

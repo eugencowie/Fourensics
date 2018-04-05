fb7a910c01af6c439a6755f7128cc7b72c335dcb
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-04-05 17:38:24 +0100

    Notify when player ready

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -129,3 +129,35 @@ exports.clueChanged = functions.database.ref('/lobbies/{id}/users/{uid}/items/{i
     });
 
 });
+
+exports.playerReady = functions.database.ref('/lobbies/{id}/users/{uid}/ready').onCreate((snapshot, context) => {
+
+    const getLobbyUserIds = "0123".split('')
+        .filter(x => x !== context.params.uid)
+        .map(x => admin.database().ref(`/lobbies/${context.params.id}/users/${x}/user-id`).once('value'));
+
+    return Promise.all(getLobbyUserIds).then(results => {
+
+        const getUserNotificationTokens = results
+            .map(x => admin.database().ref(`/users/${x.val()}/notification-token`).once('value'));
+
+        return Promise.all(getUserNotificationTokens);
+
+    }).then(results => {
+
+        const notificationTokens = results
+            .map(x => x.val())
+            .filter(x => !!x);
+
+        const payload = {
+            notification: {
+                title: 'A player is ready to vote!',
+                body: 'A player is ready to vote!'
+            }
+        };
+
+        return admin.messaging().sendToDevice(notificationTokens, payload);
+
+    });
+    
+});

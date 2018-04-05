91de8127db5e8a444868d055060451b0fac10a48
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-04-05 17:21:29 +0100

    Notify when player leaves lobby

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -34,3 +34,35 @@ exports.playerJoinedLobby = functions.database.ref('/lobbies/{id}/users/{uid}/us
     });
 
 });
+
+exports.playerLeftLobby = functions.database.ref('/lobbies/{id}/users/{uid}/user-id').onDelete((snapshot, context) => {
+
+    const getLobbyUserIds = "0123".split('')
+        .filter(x => x !== context.params.uid)
+        .map(x => admin.database().ref(`/lobbies/${context.params.id}/users/${x}/user-id`).once('value'));
+
+    return Promise.all(getLobbyUserIds).then(results => {
+
+        const getUserNotificationTokens = results
+            .map(x => admin.database().ref(`/users/${x.val()}/notification-token`).once('value'));
+
+        return Promise.all(getUserNotificationTokens);
+
+    }).then(results => {
+
+        const notificationTokens = results
+            .map(x => x.val())
+            .filter(x => !!x);
+
+        const payload = {
+            notification: {
+                title: 'Player has left the game!',
+                body: 'A player has left the lobby!'
+            }
+        };
+
+        return admin.messaging().sendToDevice(notificationTokens, payload);
+
+    });
+
+});

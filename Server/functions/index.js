0d3ed2f4802737d5a361f97df3576f29fa731b70
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-04-05 17:21:11 +0100

    Notify when player joins lobby

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -1,8 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.playerJoinedLobby = functions.database.ref('/lobbies/{id}/users/{uid}/user-id').onCreate((snapshot, context) => {
+
+    const getLobbyUserIds = "0123".split('')
+        .filter(x => x !== context.params.uid)
+        .map(x => admin.database().ref(`/lobbies/${context.params.id}/users/${x}/user-id`).once('value'));
+
+    return Promise.all(getLobbyUserIds).then(results => {
+
+        const getUserNotificationTokens = results
+            .map(x => admin.database().ref(`/users/${x.val()}/notification-token`).once('value'));
+
+        return Promise.all(getUserNotificationTokens);
+
+    }).then(results => {
+
+        const notificationTokens = results
+            .map(x => x.val())
+            .filter(x => !!x);
+
+        const payload = {
+            notification: {
+                title: 'Player has joined the game!',
+                body: 'A new player has joined the lobby!'
+            }
+        };
+
+        return admin.messaging().sendToDevice(notificationTokens, payload);
+
+    });
+
+});

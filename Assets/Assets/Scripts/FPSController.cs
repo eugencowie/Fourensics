ea79a32348688b960d835280d41a379e60a69218
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-04-09 19:42:54 +0100

    Add FPS display

--- Assets/Assets/Scripts/FPSController.cs
+++ Assets/Assets/Scripts/FPSController.cs
@@ -1,27 +1,27 @@
 using UnityEngine;
 
-public class FPSController : MonoBehaviour
+class FPSController : MonoBehaviour
 {
-    private float deltaTime = 0.0f;
+    float m_deltaTime = 0;
 
-    private void Update()
+    void Update()
     {
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        m_deltaTime += (Time.deltaTime - m_deltaTime) * 0.1f;
     }
 
     void OnGUI()
     {
-        int w = Screen.width, h = Screen.height;
+        GUIStyle style = new GUIStyle {
+            alignment = TextAnchor.UpperLeft,
+            fontSize = Screen.height * 2 / 50,
+            normal = new GUIStyleState { textColor = Color.yellow }
+        };
 
-        GUIStyle style = new GUIStyle();
-
-        Rect rect = new Rect(0, 0, w, h * 2 / 50);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 50;
-        style.normal.textColor = Color.yellow;
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
+        float msec = m_deltaTime * 1000.0f;
+        float fps = 1.0f / m_deltaTime;
         string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+
+        Rect rect = new Rect(0, 0, Screen.width, Screen.height * 2 / 50);
         GUI.Label(rect, text, style);
     }
 }

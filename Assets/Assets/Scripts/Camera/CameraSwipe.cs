583969e92d456f0256969b1a2e70bd1b369cbdcb
Eugén Cowie <eugencowie@users.noreply.github.com>
2018-04-12 10:06:16 +0100

    Implement camera swiping rotation

--- Assets/Assets/Scripts/Camera/CameraSwipe.cs
+++ Assets/Assets/Scripts/Camera/CameraSwipe.cs
@@ -1,48 +1,45 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(Camera))]
 public class CameraSwipe : MonoBehaviour
 {
     [SerializeField] bool m_enableConstraints = true;
-    [SerializeField] private int LeftConstraint = 20;
-    [SerializeField] private int RightConstraint = 255;
+    [SerializeField] int LeftConstraint = 20;
+    [SerializeField] int RightConstraint = 255;
 
-    private const float m_turnSpeed = 100.0f;
-    private Vector3 m_touchOrigin;
-    private bool m_isRotating;
+    const float m_turnSpeed = 500.0f;
+    Vector3 m_touchOrigin;
+    bool m_isTouching;
 
-    private Camera m_camera;
+    float m_velocity = 0;
 
-    private void Start()
-    {
-        m_camera = GetComponent<Camera>();
-    }
-
-    private void Update()
+    void Update()
     {
         if (Input.GetMouseButtonDown(0) && !UI.IsPointerOverUIObject())
         {
             m_touchOrigin = Input.mousePosition;
-            m_isRotating = true;
+            m_isTouching = true;
         }
 
         if (!Input.GetMouseButton(0))
         {
-            m_isRotating = false;
+            m_isTouching = false;
         }
 
-        if (m_isRotating)
+        if (m_isTouching)
         {
-            Vector3 screenPos = m_camera.ScreenToViewportPoint(Input.mousePosition - m_touchOrigin);
+            Vector3 screenPos = Camera.main.ScreenToViewportPoint(Input.mousePosition - m_touchOrigin);
+            m_velocity = screenPos.x * m_turnSpeed * Time.deltaTime * -1;
+        }
 
-            float movement = screenPos.normalized.x * m_turnSpeed * Time.deltaTime * -1;
-            
-            if (m_enableConstraints == false || (transform.rotation.eulerAngles.y > LeftConstraint - movement &&
-                transform.rotation.eulerAngles.y < RightConstraint - movement))
-            {
-                transform.RotateAround(transform.position, Vector3.up, movement);
-            }
+        if (m_enableConstraints == false || (transform.rotation.eulerAngles.y > LeftConstraint - m_velocity &&
+            transform.rotation.eulerAngles.y < RightConstraint - m_velocity))
+        {
+            transform.RotateAround(transform.position, Vector3.up, m_velocity);
+        }
+
+        if (Mathf.Abs(m_velocity) > 0.01)
+        {
+            m_velocity *= 0.9f;
         }
     }
 }
